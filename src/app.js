ba38a36aa35afb757a4478cb421d4fd7b50f78fa
Csajbok-Remenyi Laszlo <csajbokremenyilaszlo@ktch.hu>
2023-01-26 20:10:42 +0100

    Kész

--- src/app.js
+++ src/app.js
@@ -7,3 +7,25 @@
 * Github: https://github.com/Geolidth/
 * Licenc: GNU GPL
 */
+
+const asideElem = document.querySelector("#aside");
+const bsideElem = document.querySelector("#bside");
+const csideElem = document.querySelector("#cside");
+const dsideElem = document.querySelector("#dside");
+const calcButton = document.querySelector("#calculate");
+const areaElem = document.querySelector("#area");
+
+calcButton.addEventListener("click", () => {
+    console.log('Működik a kattintás!');
+    let aside=Number(asideElem.value);
+    let bside=Number(bsideElem.value);
+    let cside=Number(csideElem.value);
+    let dside=Number(dsideElem.value);
+    let area=calcArea(aside,bside,cside,dside);
+    areaElem.value=area;
+});
+
+function calcArea(a,b,c,d){
+    let terulet = ((a+c)/(4*(a-c)))*Math.sqrt((a+b-c+d)*(a-b-c+d)*(a+b-c-d)*(-a+b+c+d));
+        return terulet;
+}
