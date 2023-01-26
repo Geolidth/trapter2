ebf90de146988cd6e2d959356f10eb05924c69f0
Csajbok-Remenyi Laszlo <csajbokremenyilaszlo@ktch.hu>
2023-01-26 19:01:16 +0100

    Beviteli mezők létrehozása

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/*
+* File: app.js
+* Author: Csajbók-Reményi László
+* Copyright: 2023, Csajbók-Reményi László
+* Group: Szoft I/2/E
+* Date: 2023-01-26
+* Github: https://github.com/Geolidth/
+* Licenc: GNU GPL
+*/
